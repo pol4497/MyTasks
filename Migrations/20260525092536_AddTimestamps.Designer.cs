8754f1dc3fa4576e6beddc305e57c9ff58058f71
Politidis Thodoris <thodoris_politidis@yahoo.gr>
2026-05-29 06:54:26 +0300

    Add migration for timestamps

--- Migrations/20260525092536_AddTimestamps.Designer.cs
+++ Migrations/20260525092536_AddTimestamps.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyTasks.Data;
 
@@ -10,9 +11,11 @@ using MyTasks.Data;
 namespace MyTasks.Migrations
 {
     [DbContext(typeof(MyTasksContext))]
-    partial class MyTasksContextModelSnapshot : ModelSnapshot
+    [Migration("20260525092536_AddTimestamps")]
+    partial class AddTimestamps
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.9");
---
+++ Migrations/20260525092536_AddTimestamps.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyTasks.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTimestamps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "TaskItems",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "TaskItems",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "TaskItems");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "TaskItems");
+        }
+    }
+}
