e384c98e6ace5287d26b1eabe3bd294b992a0534
Politidis Thodoris <thodoris_politidis@yahoo.gr>
2026-08-13 15:32:27 +0300

    chore: add EF Core migration enforcing the task-owner check constraint

---
+++ Migrations/20260813104001_AddTaskOwnerXorConstraint.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyTasks.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTaskOwnerXorConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_TaskItem_OwnerXor",
+                table: "TaskItems",
+                sql: "((UserId IS NOT NULL AND GuestSessionId IS NULL) OR (UserId IS NULL AND GuestSessionId IS NOT NULL))");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_TaskItem_OwnerXor",
+                table: "TaskItems");
+        }
+    }
+}
